b02bf6ea7d7e8dad6e7e3f0edb8adf6aab360526
Quentin Sommesous <quentin.sommesous@sfr.fr>
2022-01-04 16:59:44 +0100

    reimportation du css et js du site precedent

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,150 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-// start the Stimulus application
-import './bootstrap';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import "bootstrap"
+import 'datatables.net'
+import 'datatables.net-bs5'
+
+require('jquery-ui/ui/widgets/droppable');
+require('jquery-ui/ui/widgets/sortable');
+require('jquery-ui/ui/widgets/selectable');
+import $ from "jquery"
+
+$("#datatable").DataTable({
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.10.15/i18n/French.json'
+    },
+    pageLength: 50,
+    autoWidth: false
+})
+$(function () {
+    $("#sortable").sortable({
+        placeholder: "fantom",
+        update: function (event, ui) {
+            const list = $("tbody#sortable")
+            let pos = 0;
+            $(list.find("tr")).each(function () {
+                pos++
+                $(this).find("input.positionInput").val(pos)
+                $(this).find(".positionBrut").html(pos)
+            })
+        }
+    });
+});
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'image code',
+    toolbar: 'undo redo | link image | code',
+    image_title: true,
+    automatic_uploads: true,
+    file_picker_types: 'image',
+    file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+        input.onchange = function () {
+            var file = this.files[0];
+            var reader = new FileReader();
+            reader.onload = function () {
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+                cb(blobInfo.blobUri(), {title: file.name});
+            };
+            reader.readAsDataURL(file);
+        };
+        input.click();
+    }
+});
+
+$(document).ready(() => {
+    let nomination = $(".section-text")
+    let price = $(".section-price")
+    let createSection = $(".section-inge")
+    let options = $(".section-options")
+    let attachment = $(".section-download")
+    let meta = $(".section-meta")
+    let declination = $(".section-declination")
+    let productItems = $(".product-item")
+    let productSections = $(".product-section")
+
+    $(".item-text").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        nomination.show()
+        $(".product-text").addClass('table-active')
+    })
+    $(".item-price").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        price.show()
+        $(".product-price").addClass('table-active')
+    })
+    $(".item-inge").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        createSection.show()
+        $(".product-inge").addClass('table-active')
+    })
+    $(".item-download").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        attachment.show()
+        $(".product-download").addClass('table-active')
+    })
+    $(".item-options").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        options.show()
+        $(".product-options").addClass('table-active')
+    })
+
+    $(".item-meta").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        meta.show()
+        $(".product-meta").addClass('table-active')
+    })
+
+    $(".item-declination").click(() => {
+        productItems.removeClass("table-active")
+        productSections.hide()
+        declination.show()
+        $(".product-meta").addClass('table-active')
+    })
+})
+
+$(".add-declination").click(() => {
+    $('.formDeclination').toggle()
+})
+
+function appear(element) {
+    return element.slideToggle()
+}
+
+const testimonials = $('#texts')
+    .children()
+    .filter('div.text');
+
+const showTestimonial = index => {
+
+    testimonials.hide();
+    $(testimonials[index]).fadeIn();
+
+    return index === testimonials.length
+        ? showTestimonial(0)
+        : setTimeout(() => {
+            showTestimonial(index + 1);
+        }, 5000);
+}
+
+showTestimonial(0); // id of the first element you want to show.
+
+$(".fa-caret-down").click(() => {
+    $(".menu-nav").toggle(500)
+    $(".fa-caret-down").toggleClass("rotate")
+})
---
+++ src/Controller/LayoutBundleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\AttributeGroupRepository;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class LayoutBundleController extends AbstractController
+{
+    public function adminLayoutLeft(): Response
+    {
+        $uri = $_SERVER["REQUEST_URI"];
+        return $this->render("admin/layout/_leftColumnAdmin.html.twig", [
+            "uri" => $uri
+        ]);
+    }
+
+    public function adminLayoutHeader(): Response
+    {
+        return $this->render("admin/layout/_header.html.twig", [
+
+        ]);
+    }
+
+    public function publicLayoutHeader(): Response
+    {
+        return $this->render("layout/_header.html.twig", []);
+    }
+}
